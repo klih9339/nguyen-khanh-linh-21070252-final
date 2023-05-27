056858d44896ce166346cf8af0229c90fa049e45
klih9339 <21070252@vnu.edu.vn>
2023-05-27 13:53:51 +0700

    new

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ApplicationRecord
 	has_many:orders
 	validates_presence_of:employee_name, :employee_dob,:employee_position
-	
+	validates_numericality_of:employee_salary
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :category
-  
+  validates_numericality_of:price
 end
