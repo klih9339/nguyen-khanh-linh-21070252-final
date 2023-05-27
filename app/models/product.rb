84c33fad131e4b4be77d9a47f71721f58c328d72
klih9339 <21070252@vnu.edu.vn>
2023-05-27 14:42:02 +0700

    m

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
   validates_presence_of:product_name,:brand,:category
+  validates_uniqueness_of:product_name
   validates_numericality_of:price
 end
